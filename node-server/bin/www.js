400a26d232a25d2f6f78e42f52cd609b8979c11b
unknown <sjgrbent40@gmail.com>
2020-01-12 09:59:32 +0200

    fixed routes, needs working selects

--- node-server/bin/www.js
+++ node-server/bin/www.js
@@ -2,5 +2,5 @@ const app = require("../src/app")
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('server is up on port 5000');
+    console.log(`server is up on port ${port}`);
 });
